e941d48d78c6aff89785ff5adce2660ecf747c36
mrsmothers <mrsmothers@gmail.com>
2014-06-05 22:06:05 -0700

    Update imageComparer.java

--- imageComparer.java
+++ imageComparer.java
@@ -2,6 +2,7 @@ import java.awt.Image;
 import java.awt.image.*;
 import java.io.*;
 import javax.imageio.*;
+import javax.vecmath.Vector3d;
 
 class imageComparer {
   public static void main(String[] args){
@@ -11,12 +12,20 @@ class imageComparer {
   }
   
   public static double comparer(BufferedImage img1, BufferedImage img2){
-    double number=0;
+    double sum=0;
     
     for(int I = 0;I<img1.getHeight(); I++){
       for(int J = 0; J<img1.getWidth(); J++){
-        number+= Math.pow(
-    return 0.0;
+        Vector3d v1,v2;
+        v1 = quantizePixle(img1,I,J);
+        v2 = quantizePixle(img2,I,J);
+        
+        v1.sub(v2);
+        sum+=v1.length();
+      }
+    }
+
+    return sum/(img1.getWidth()*img2.getHeight()*256);
   }
 
 
@@ -26,39 +35,17 @@ class imageComparer {
        } catch (IOException e) { }
        
   }
+  
+  public static double[] quantizePixle(BufferedImage img, int x, int y){
+        int clr=  img.getRGB(x, y); 
+    int  red   = (clr & 0x00ff0000) >> 16;
+    int  green = (clr & 0x0000ff00) >> 8;
+    int  blue  =  clr & 0x000000ff;
+    
+    Vector3d out = new Vector3d(red, green, blue);
+    
+    return out;
+  }
 
 
-}
-
-
-
-import java.awt.*;
-import java.awt.image.BufferedImage;
-import java.io.*;
-
-import javax.imageio.ImageIO;
-import javax.swing.ImageIcon;
-
-
-class Main {
-    public static void main(String[] args) throws IOException {
-
-        final int SCALE = 2;
-
-        Image img = new ImageIcon("duke.png").getImage();
-
-        BufferedImage bi = new BufferedImage(SCALE * img.getWidth(null),
-                                             SCALE * img.getHeight(null),
-                                             BufferedImage.TYPE_INT_ARGB);
-
-        Graphics2D grph = (Graphics2D) bi.getGraphics();
-        grph.scale(SCALE, SCALE);
-
-        // everything drawn with grph from now on will get scaled.
-
-        grph.drawImage(img, 0, 0, null);
-        grph.dispose();
-
-        ImageIO.write(bi, "png", new File("duke_double_size.png"));
-    }
 }
