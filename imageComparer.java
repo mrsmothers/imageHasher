6bd9bc2326e8e3579f79cd4ddac9847e857b2aea
mrsmothers <mrsmothers@gmail.com>
2014-06-29 18:32:17 -0700

    Update imageComparer.java

--- imageComparer.java
+++ imageComparer.java
@@ -6,18 +6,18 @@ import javax.imageio.*;
 class imageComparer {
   
   public static void main(String[] args){
-    System.out.println(comparer(args[0], args[1]);
+    System.out.println(comparer(openImageFile(args[0]), openImageFile(args[1])));
   }
   //campare each
   public static double comparer(BufferedImage img1, BufferedImage img2){
     double sum = 0;
     
-    for(int I = 0; I < img1.getHeight(); I++){
-      for(int J = 0; J < img1.getWidth(); J++){
+    for(int I = 0; I < ((img1.getHeight() < img2.getHeight()) ? img1.getHeight() : img2.getHeight()); I++){
+      for(int J = 0; J < ((img1.getWidth() < img2.getWidth()) ? img1.getWidth() : img2.getWidth()); J++){
         float[] v1, v2 ,delta;
         
-        v1 = quantizePixle(img1, I, J);
-        v2 = quantizePixle(img2, I, J);
+        v1 = quantizePixle(img1, J, I);
+        v2 = quantizePixle(img2, J, I);
         
         delta = vSub(v1, v2);
         
@@ -29,19 +29,23 @@ class imageComparer {
   }
 
 
-  public static void openImageFile(String fileName){
+  public static BufferedImage openImageFile(String fileName){
+	BufferedImage img = null;
+
        try {
            img = ImageIO.read(new File(fileName));
-       } catch (IOException e) { }
+       } catch (IOException e) {System.out.println("file not found "+fileName); }
+
+	return img;
   }
   
   public static float[] quantizePixle(BufferedImage img, int x, int y){
-    int clr   =  img.getRGB(x, y); 
-    int red   = (clr & 0x00ff0000) >> 16;
+    int clr = img.getRGB(x,y);
+    int red = (clr & 0x00ff0000) >> 16;
     int green = (clr & 0x0000ff00) >> 8;
-    int blue  =  clr & 0x000000ff;
+    int blue = clr & 0x000000ff;
     
-    float[]  out = {red, green, blue};
+    float[] out = {red, green, blue};
     
     return out;
   }
@@ -52,7 +56,7 @@ class imageComparer {
     for(int I=0; I<vector.length; I++)
       sum += Math.pow(vector[I], 2);
       
-    return Math.pow(sum, 0.5);
+    return (float)Math.pow(sum, 0.5);
   }
   
   public static float[] vAdd(float[] arg1, float[] arg2){
