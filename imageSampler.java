39698229794dd3902823428f5db3ddbd42f483ee
mrsmothers <mrsmothers@gmail.com>
2014-07-02 16:49:48 -0700

    Update imageSampler.java

--- imageSampler.java
+++ imageSampler.java
@@ -9,13 +9,21 @@ public class imageSampler{
 //compare with data base
 
     public BufferedImage originalImage;
-    public signiture;
+    public String signiture;
     public float[] kernal, histogram;
     public String[] dataBase;
     
     
     public String[] returnMatches(){
         BufferedImage grayImg = intesityMap.remap(originalImage);
+        
+        int boxWidth;
+        
+        //box portions of the screen to prosses
+        for(int I = 0; I<kernal.length; I++)//for each field of view use a different boxWidth
+        for(int J = 0; J<originalImage.length+boxWidth; J++)// for the length of the image
+        for(int K = 0; K<originalImage.length+boxWidth;K++)
+        
         
         
     }
