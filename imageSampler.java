c8808257dec7a40cc94090a5c4b5d65ebb766cbc
mrsmothers <mrsmothers@gmail.com>
2014-07-02 15:48:46 -0700

    Update and rename imageSampler to imageSampler.java

---
+++ imageSampler.java
@@ -0,0 +1,24 @@
+//returns a subsample of a BufferedImage
+
+
+public class imageSampler{
+
+//select a portion of the screen
+//generate histograme of with prossed image(gassian blure, and grayscale conversion)
+//charicterize hisogame signiture
+//compare with data base
+
+    public BufferedImage originalImage;
+    public signiture;
+    public float[] kernal, histogram;
+    public String[] dataBase;
+    
+    
+    public String[] returnMatches(){
+        BufferedImage grayImg = intesityMap.remap(originalImage);
+        
+        
+    }
+
+  
+}
