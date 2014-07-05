6e2ac6c8da939811927b8115137404c3f74ba8b6
mrsmothers <mrsmothers@gmail.com>
2014-07-05 14:21:37 -0700

    Update imageHistogram.java

--- imageHistogram.java
+++ imageHistogram.java
@@ -9,41 +9,51 @@ import java.lang.Math;
 
 
 public class imageHistogram{
-  float[] histogram;
-  int mode;
+	int mode;
+	int entries;
+	int[] histogram;
   
   
-  public static void main(String[] args){
-    Bufferedimage img = openImageFile(args[0]);
-    int work[] = imageHistogram.prosses(img);
-    
-    for(int I=0; I<tmp.length; I++){
-      System.out.println(I+":"+work[I]);
-    }
-  }
   
-  public static int[] prosses(BufferedImage img){
-    int out = new int[256];
+	public static void main(String[] args){
+		BufferedImage img = openImageFile(args[0]);
+		imageHistogram ih = new imageHistogram(img);
+	
+		System.out.println(ih.print(50));
+
+  	}
+
+	public imageHistogram(BufferedImage img){
+		mode = 0;
+		entries++;
+		histogram = prosses(img);
+
+	}	
+  
+  public int[] prosses(BufferedImage img){
+    int[] out = new int[256];
     
     Arrays.fill(out, 0);
     
     for(int I = 0; I < img.getHeight(); I++){
       for(int J = 0; J < img.getWidth(); J++){
         int[] v = quantizePixle(img, J, I);
-        int tmp = (int)(vLength(v)/(Math.pow(3, 0.5)));
+	 float[] vnu = { v[0], v[1], v[2]};
+        int tmp = (int)(vLength((vnu))/(Math.pow(3, 0.5)));
 
         out[tmp]++;
-        
-        if(out[tmp] >out[mode])
-        mode = tmp;
+		enteies++;
+
+		if(out[tmp] > out[mode])
+			mode = tmp;
       }
     }
     
     return out;
   }
   
-    public static BufferedImage openImageFile(String fileName){
-     BufferedImage img;
+  public static BufferedImage openImageFile(String fileName){
+     BufferedImage img = null;
     
      try {
        img = ImageIO.read(new File(fileName));
@@ -53,41 +63,53 @@ public class imageHistogram{
        
   }
 
-    public static int[] quantizePixle(BufferedImage img, int x, int y){
-        int clr = img.getRGB(x, y);
-        int red = (clr & 0x00ff0000) >> 16;
-       int green = (clr & 0x0000ff00) >> 8;
-          int blue = clr & 0x000000ff;
+  public static int[] quantizePixle(BufferedImage img, int x, int y){
+    int clr = img.getRGB(x, y);
+    int red = (clr & 0x00ff0000) >> 16;
+    int green = (clr & 0x0000ff00) >> 8;
+    int blue = clr & 0x000000ff;
     
-     int[] out = {red, green, blue};
+    int[] out = {red, green, blue};
     
-      return out;
+    return out;
+  } 
+
+  public static float vLength(float[] vector){
+    double sum = 0;
+    
+    for(int I=0; I<vector.length; I++)
+      sum += Math.pow(vector[I], 2);
+      
+    return (float)Math.pow(sum, 0.5);
   } 
   
      public String print(int width){
          StringBuilder out = new StringBuilder();
-         
+         out.append("   |");
          for(int I=0;I<width;I++)
-            out.append(""+(I+1)%10));
+            out.append(""+((I+1)%10));
             
         out.append('\n');
         
 
     
-    for(int I = 0; I< histogram.length; I++)
-        out.append(I+1);
-        switch((int)Math.log10(I)){
-            case 0:
-                out.append("  :");
-                break;
-            case 1:
-                out.append(" :");
-                break;
-            default:
-               out.append(':');
-        }
-        for(int K = 0; K < (int)(histograme[I]/histogram[mode]); K++)
-        out.append('.')
-    }
-      out.append('\n');
+		for(int I = 0; I < histogram.length; I++){
+        	out.append(I+1);
+        	switch((int)Math.log10(I+1)){
+            	case 0:
+                	out.append("  |");
+                	break;
+            	case 1:
+                	out.append(" |");
+               		break;
+            	default:
+               		out.append('|');
+        	}
+        	for(int K = 0; K < (int)(width*histogram[I]/histogram[mode]); K++){
+        		out.append('.');
+			}
+			out.append('\n');
+		}
+		return out.toString();
+	}
 }
