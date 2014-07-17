4dabde5ad42e36857974d098d38489b86af2e4ff
mrsmothers <mrsmothers@gmail.com>
2014-07-16 23:31:24 -0700

    Update HistogramImageHash.java

--- HistogramImageHash.java
+++ HistogramImageHash.java
@@ -7,21 +7,20 @@ public class HistogramImageHash{
 
 	public static float[][] prosses(BufferedImage img){
 		float out[] = new float[3][256];
-		BufferedImage gradiant = imageGradient(img);
+		BufferedImage gradient = imageGradient(img);
 
 		float[] kernal = ImageHasher.gassianKernal((img.getWidth()-1)/2, 1);
 		
 		int kernalIndex = 0;
 		for(int I = 0; I < img.getWidth();I++){
 			for(int K = 0; K < img.getWidth(); K++){
-				out[0][(gradient.getRGB(I , K) & x00ff0000) >> 16] += kernal[kernalIndex];
-				out[1][(gradient.getRGB(I , K + img.getHeight()/5) & x00ff0000) >> 16] += kernal[kernalIndex];
-				out[2][(gradient.getRGB(I , K + 2*img.getHeight()/5-1) & x00ff0000) >> 16] += kernal[kernalIndex];
+				out[0][((gradient.getRGB(I , K) & x00ff0000) >> 16)] += kernal[kernalIndex];
+				out[1][((gradient.getRGB(I , K + img.getHeight()/5) & x00ff0000) >> 16)] += kernal[kernalIndex];
+				out[2][((gradient.getRGB(I , K + 2*img.getHeight()/5-1) & x00ff0000) >> 16)] += kernal[kernalIndex];
 				kernalIndex++;
 			}
 		}
 
-		return prosses;
+		return out;
 }
-
 
