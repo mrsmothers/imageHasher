20419c51dca970f3c5b66aacf168b31b6412c5b2
mrsmothers <mrsmothers@gmail.com>
2014-07-17 20:43:01 -0700

    Update HistogramImageHash.java

--- HistogramImageHash.java
+++ HistogramImageHash.java
@@ -1,26 +1,28 @@
-import java.awt.image.*;
-import java.awt.Image.BufferedImage;
+import java.awt.Image;
+import java.awt.image.BufferedImage;
+import java.util.*;
 
 
 public class HistogramImageHash{
 
 
 	public static float[][] prosses(BufferedImage img){
-		float out[] = new float[3][256];
-		BufferedImage gradient = imageGradient(img);
-
-		float[] kernal = ImageHasher.gassianKernal((img.getWidth()-1)/2, 1);
+		float out[][] = new float[3][256];
+		BufferedImage gradient = ImageGradient.prosses(img);
+		
+		int kernalWidth = (img.getWidth()-1)/2;
+		float[] kernal = ImageHasher.gassianKernel(kernalWidth , 1f);
 		
 		int kernalIndex = 0;
-		for(int I = 0; I < img.getWidth();I++){
-			for(int K = 0; K < img.getWidth(); K++){
-				out[0][((gradient.getRGB(I , K) & x00ff0000) >> 16)] += kernal[kernalIndex];
-				out[1][((gradient.getRGB(I , K + (img.getHeight()-img.getWidth())/2) & x00ff0000) >> 16)] += kernal[kernalIndex];
-				out[2][((gradient.getRGB(I , K + (img.getHeight()-img.getWidth())-1) & x00ff0000) >> 16)] += kernal[kernalIndex];
+		for(int I = 0; I < kernalWidth;I++){
+			for(int K = 0; K < kernalWidth; K++){
+				out[0][((gradient.getRGB(I , K) & 0x00ff0000) >> 16)] += kernal[kernalIndex];
+				out[1][((gradient.getRGB(I , K + (img.getHeight()-img.getWidth())/2) & 0x00ff0000) >> 16)] += kernal[kernalIndex];
+				out[2][((gradient.getRGB(I , K + (img.getHeight()-img.getWidth())-1) & 0x00ff0000) >> 16)] += kernal[kernalIndex];
 				kernalIndex++;
 			}
 		}
-
+		System.out.println(Arrays.toString(kernal));
 		return out;
+	}
 }
-
